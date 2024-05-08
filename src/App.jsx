aa0fc61e93981a97045623c32e744b8c0691919b
Sarthak <00217702722_cse@vips.edu>
2024-05-08 22:00:02 +0530

    external build

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ import Rocket from "./assets/Rocket.svg";
 import QuestionBox from "./Components/QuestionBox/QuestionBox";
 import "./App.css";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import { faArrowRight, faCopyright } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function App() {
   return (
     <div className="h-screen w-screen overflow-x-hidden m-0 p-0 flex flex-col items-center ">
