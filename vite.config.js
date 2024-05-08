1453fad1b1ba327ec0407f85346c77a7244883b1
Sarthak <00217702722_cse@vips.edu>
2024-05-08 22:07:56 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
-
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['@fortawesome/free-solid-svg-icons']
-    }
+  plugins: [react()],
+ 
+    build: {
+      rollupOptions: {
+        external: ["@fortawesome/react-fontawesome"],
+      },
+
   }
-});
+})
